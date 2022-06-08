61190a311fbcbee7031fd95fd503867509b9346a
Mashiro <inoriheart@gmail.com>
2022-06-08 12:37:17 +0900

    Wed Jun  8 12:37:17 PM JST 2022

--- BuildEmacs-x86_64.sh
+++ BuildEmacs-x86_64.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-rm -rf /Users/mashiro/Library/Caches/makepkg/emacs-mashiro/pkg
-rm -rf /Users/mashiro/Library/Caches/makepkg/emacs-mashiro/src
-
-cd /Users/mashiro/Library/Caches/makepkg/emacs-mashiro/
-
+cd /Users/mashiro/Library/Caches/mashiro/emacs/
 makepkg
 
 rm /Users/mashiro/Library/Caches/mashiro/x86_64/emacs*.pkg.tar.zst
+mv /Users/mashiro/Library/Caches/mashiro/emacs/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
 
-mv /Users/mashiro/Library/Caches/makepkg/emacs-mashiro/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
+rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/pkg
+rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/src
