8500a21c3df64707ec5ba10a1245197cbb6b33dc
Mashiro <inoriheart@gmail.com>
2022-06-19 20:49:47 +0900

    Sun Jun 19 08:49:47 PM JST 2022

--- BuildEmacs-x86_64.sh
+++ BuildEmacs-x86_64.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
+rm /Users/mashiro/Library/Caches/mashiro/x86_64/emacs*.pkg.tar.zst
+
 cd /Users/mashiro/Library/Caches/mashiro/emacs/
 makepkg
-
-cd /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/
-makepkg
-
-rm /Users/mashiro/Library/Caches/mashiro/x86_64/emacs*.pkg.tar.zst
 mv /Users/mashiro/Library/Caches/mashiro/emacs/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
-mv /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
-
 rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/pkg
 rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/src
 
-rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/pkg
-rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/src
+# cd /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/
+# makepkg
+# mv /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
+# rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/pkg
+# rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/src
--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -3,4 +3,5 @@
 repo-add /Users/mashiro/Library/Caches/mashiro/aarch64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/aarch64/*.zst
 repo-add /Users/mashiro/Library/Caches/mashiro/x86_64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/x86_64/*.zst
 
-rsync -azP -e 'ssh -p 2222' --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.200:/volume1/home/mashiro/www/
+rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
+rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.211:/srv/http
