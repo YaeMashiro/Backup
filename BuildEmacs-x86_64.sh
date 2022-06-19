b6f9235909a4f9cece5ffb14d26c5bbc09848a72
Mashiro <inoriheart@gmail.com>
2022-06-19 09:32:02 +0900

    Sun Jun 19 09:32:02 AM JST 2022

--- BuildEmacs-x86_64.sh
+++ BuildEmacs-x86_64.sh
@@ -3,8 +3,15 @@
 cd /Users/mashiro/Library/Caches/mashiro/emacs/
 makepkg
 
+cd /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/
+makepkg
+
 rm /Users/mashiro/Library/Caches/mashiro/x86_64/emacs*.pkg.tar.zst
 mv /Users/mashiro/Library/Caches/mashiro/emacs/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
+mv /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
 
 rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/pkg
 rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/src
+
+rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/pkg
+rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/src
