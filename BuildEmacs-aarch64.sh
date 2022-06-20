f13408dd8af3acb8b02abf349b9ad05511b257cf
Mashiro <inoriheart@gmail.com>
2022-06-20 22:26:11 +0900

    Mon Jun 20 10:26:11 PM JST 2022

--- BuildEmacs-aarch64.sh
+++ BuildEmacs-aarch64.sh
@@ -7,13 +7,12 @@ if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/emacs-nox-mashiro-29.0.50
     cd /Users/mashiro/Library/Caches/mashiro/emacs-nox/
     makepkg
 
-    rm /Users/mashiro/Library/Caches/mashiro/aarch64/emacs*.tar.zst
-    cp /Users/mashiro/Library/Caches/mashiro/emacs-nox/*.zst /Users/mashiro/Library/Caches/mashiro/aarch64/
+    rm /Users/mashiro/Library/Caches/mashiro/aarch64/emacs*.pkg.tar.zst
+    mv /Users/mashiro/Library/Caches/mashiro/emacs-nox/*.zst /Users/mashiro/Library/Caches/mashiro/aarch64/
 
-    ssh mashiro@15.155.2.5 'rm /Users/mashiro/Library/Caches/mashiro/aarch64/emacs*.tar.zst'
-    scp /Users/mashiro/Library/Caches/mashiro/emacs-nox/*.zst mashiro@15.155.2.5:/Users/mashiro/Library/Caches/mashiro/aarch64/
+    ssh mashiro@15.155.2.5 'rm /Users/mashiro/Library/Caches/mashiro/aarch64/emacs*.pkg.tar.zst'
+    scp /Users/mashiro/Library/Caches/mashiro/aarch64/*.zst mashiro@15.155.2.5:/Users/mashiro/Library/Caches/mashiro/aarch64/
 
-    rm /Users/mashiro/Library/Caches/mashiro/emacs-nox/emacs*.tar.zst
     rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-nox/pkg
     rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-nox/src
 fi
