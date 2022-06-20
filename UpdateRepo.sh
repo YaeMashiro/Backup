7ce4beae74b41f330d65d45d9eb41b5c644edc9c
Mashiro <inoriheart@gmail.com>
2022-06-20 22:15:47 +0900

    Mon Jun 20 10:15:47 PM JST 2022

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-repo-add /Users/mashiro/Library/Caches/mashiro/aarch64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/aarch64/*.zst
-repo-add /Users/mashiro/Library/Caches/mashiro/x86_64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/x86_64/*.zst
 
-rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
-rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.211:/srv/http
+if [ -f "/Users/mashiro/Library/Caches/mashiro/x86_64/emacs-mashiro-29.0.50.$(date "+%Y%m%d")-1-x86_64.pkg.tar.zst" ]
+  then
+    repo-add /Users/mashiro/Library/Caches/mashiro/x86_64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/x86_64/*.zst
+    rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
+    rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.211:/srv/http
+  else
+    echo "NyaNya"
+fi
+
+if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/emacs-mashiro-29.0.50.$(date "+%Y%m%d")-1-aarch64.pkg.tar.zst" ]
+  then
+    repo-add /Users/mashiro/Library/Caches/mashiro/aarch64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/aarch64/*.zst
+    rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
+    rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.211:/srv/http
+  else
+    echo "NyaNya-aarch64"
+fi
+
