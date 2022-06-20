c230fd9594dd472e808b9a1c712a493b7b935d44
Mashiro <inoriheart@gmail.com>
2022-06-20 22:07:42 +0900

    Mon Jun 20 10:07:42 PM JST 2022

--- BuildEmacs-x86_64.sh
+++ BuildEmacs-x86_64.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-rm /Users/mashiro/Library/Caches/mashiro/x86_64/emacs*.pkg.tar.zst
+if [ -f "/Users/mashiro/Library/Caches/mashiro/x86_64/emacs-mashiro-29.0.50.$(date "+%Y%m%d")-1-x86_64.pkg.tar.zst" ]
+  then
+    echo "Nya"
+  else
+    rm /Users/mashiro/Library/Caches/mashiro/x86_64/emacs*.pkg.tar.zst
 
-cd /Users/mashiro/Library/Caches/mashiro/emacs/
-makepkg
-mv /Users/mashiro/Library/Caches/mashiro/emacs/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
-rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/pkg
-rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/src
+    cd /Users/mashiro/Library/Caches/mashiro/emacs/
+    makepkg
+    mv /Users/mashiro/Library/Caches/mashiro/emacs/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
+    rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/pkg
+    rm -rf /Users/mashiro/Library/Caches/mashiro/emacs/src
 
-# cd /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/
-# makepkg
-# mv /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
-# rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/pkg
-# rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/src
+   # cd /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/
+   # makepkg
+   # mv /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/*.zst /Users/mashiro/Library/Caches/mashiro/x86_64/
+   # rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/pkg
+   # rm -rf /Users/mashiro/Library/Caches/mashiro/emacs-pgtk/src
+fi
