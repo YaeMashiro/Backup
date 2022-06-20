2d06ca90711028aa32a92ca94c4b81818a643535
Mashiro <inoriheart@gmail.com>
2022-06-20 22:17:21 +0900

    Mon Jun 20 10:17:21 PM JST 2022

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -10,7 +10,7 @@ if [ -f "/Users/mashiro/Library/Caches/mashiro/x86_64/emacs-mashiro-29.0.50.$(da
     echo "NyaNya"
 fi
 
-if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/emacs-mashiro-29.0.50.$(date "+%Y%m%d")-1-aarch64.pkg.tar.zst" ]
+if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/emacs-nox-mashiro-29.0.50.$(date "+%Y%m%d")-1-aarch64.pkg.tar.zst" ]
   then
     repo-add /Users/mashiro/Library/Caches/mashiro/aarch64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/aarch64/*.zst
     rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
