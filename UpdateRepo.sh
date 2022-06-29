41ceab9f327f322afc990db58b906ded6440cde7
Mashiro <inoriheart@gmail.com>
2022-06-29 20:37:31 +0900

    Wed Jun 29 08:37:31 PM JST 2022

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -4,17 +4,17 @@
 if [ -f "/Users/mashiro/Library/Caches/mashiro/x86_64/emacs-mashiro-29.0.50-$(date "+%Y%m%d").1-x86_64.pkg.tar.zst" ]
   then
     repo-add /Users/mashiro/Library/Caches/mashiro/x86_64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/x86_64/*.zst
-    #rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
     rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.211:/srv/http
+    #rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
   else
-    echo "NyaNya"
+    echo "Nya"
 fi
 
 if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/emacs-nox-mashiro-29.0.50-$(date "+%Y%m%d").1-aarch64.pkg.tar.zst" ]
   then
     repo-add /Users/mashiro/Library/Caches/mashiro/aarch64/mashiro.db.tar.gz /Users/mashiro/Library/Caches/mashiro/aarch64/*.zst
-    #rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
     rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.211:/srv/http
+    #rsync -azP --delete /Users/mashiro/Library/Caches/mashiro mashiro@15.155.2.215:/srv/http
   else
-    echo "NyaNya"
+    echo "Nya"
 fi
